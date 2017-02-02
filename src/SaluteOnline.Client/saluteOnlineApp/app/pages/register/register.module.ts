7c3aa1bfdacbeef8a97380cddfab469d4dc173fc
TD <melomaniac.taras@gmail.com>
2017-02-03 00:56:33 +0200

    registration form

--- src/SaluteOnline.Client/saluteOnlineApp/app/pages/register/register.module.ts
+++ src/SaluteOnline.Client/saluteOnlineApp/app/pages/register/register.module.ts
@@ -1,13 +1,13 @@
 ﻿import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MdInputModule } from "@angular/material";
+import { MdInputModule, MdButtonModule } from "@angular/material";
 
 import { SoRegister } from "./register.component";
 import { routing } from "./register.routing";
 
 @NgModule({
-    imports: [CommonModule, ReactiveFormsModule, FormsModule, MdInputModule, routing],
+    imports: [CommonModule, ReactiveFormsModule, FormsModule, MdInputModule, MdButtonModule, routing],
     declarations: [SoRegister]
 })
 export class RegisterModule {
